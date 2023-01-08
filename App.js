66c307b2d1fa52f71ada92f4653e0e963044111a
Ruslan Gaziev <grinrus@yandex.ru>
2023-01-08 22:35:35 +0300

    Lesson 3

--- App.js
+++ App.js
@@ -1,20 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, Button, Alert, Image, TouchableWithoutFeedback } from 'react-native';
 
 export default function App() {
+  const handleTextPress = () => console.log('Press')
+
+  const handleButtonPress = () => Alert.alert("MyApp", "Main message", [
+    {text: "Yes", onPress: () => console.log('Yes')},
+    {text: "No", onPress: () => console.log('No')}
+  ])
+
+  const handleButtonPress2 = () => Alert.prompt("MyApp", "Main message", text => console.log(text))
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.text} onPress={handleTextPress}>Hello!</Text>
+      <Button title={'Press on me'} onPress={handleButtonPress} />
+      <Button title={'New button'} onPress={handleButtonPress2} />
+      <Image source={require('./assets/icon.png')} style={styles.image} />
+      <TouchableWithoutFeedback onPress={handleButtonPress2}>
+           <Image blurRadius={5} source={{uri: "https://cdn.britannica.com/46/154246-050-7C72E12F/view-Rome.jpg"}} style={styles.image} />
+      </TouchableWithoutFeedback>
+   
+      
+
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#fff'
   },
+  text: {
+    color: 'red',
+  },
+  image: {
+    width: 200,
+    height: 200
+  }
 });
